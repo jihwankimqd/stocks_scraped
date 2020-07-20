65b8888d576c044992479c558fc179fccbb346da
Ji Hwan Kim <jihwankimqd@gmail.com>
2020-07-20 14:07:45 +0900

    first

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ import pandas_datareader.data as web
 from datetime import datetime as dt
 from balance_sheet_scraper import get_balance_sheet
 
-# ###
-# url = 'https://raw.githubusercontent.com/jihwankimqd/stocks_scraped/master/kospi_data.csv'
-# df = pd.read_csv(url,sep=",")
-# col = ['기업명', '종목코드']
-# df1 = df[col].copy()
-# df1.columns = ['label','value']
-# df1['value'] = df1['value'].apply(str)
-# df1['value'] = df1['value'].str.zfill(6)
-# df1['label'] = df1['label']+' ('+df1['value']+')'
-# stock_data = df1.to_dict('records')
-# ###
+###
+url = 'https://raw.githubusercontent.com/jihwankimqd/stocks_scraped/master/kospi_data.csv'
+df = pd.read_csv(url,sep=",")
+col = ['기업명', '종목코드']
+df1 = df[col].copy()
+df1.columns = ['label','value']
+df1['value'] = df1['value'].apply(str)
+df1['value'] = df1['value'].str.zfill(6)
+df1['label'] = df1['label']+' ('+df1['value']+')'
+stock_data = df1.to_dict('records')
+###
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -41,11 +41,11 @@ app.layout = html.Div([
         html.H2('Choose a Stock Ticker'),
         dcc.Dropdown(
             id='my-dropdown',
-            options=[
-            {'label': 'Samsung', 'value': '005930'},
-            {'label': 'SKInnovation', 'value': '096770'}
-        ],
-            # options=stock_data,
+        #     options=[
+        #     {'label': 'Samsung', 'value': '005930'},
+        #     {'label': 'SKInnovation', 'value': '096770'}
+        # ],
+            options=stock_data,
             value='005930'
         ),
     
@@ -107,16 +107,16 @@ def generate_balance_table(stock_id, max_rows=16):
     ])
 
 # Stock Graph
-@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
-def update_graph(stock_id):
-    dataframe = web.DataReader(stock_id, 'naver', start='2015-01-01', end=dt.now()).reset_index()
-    dataframe['Date'] = pd.to_datetime(dataframe['Date']).dt.date
-    return {
-        'data': [{
-            'x': dataframe.Date,
-            'y': dataframe.Close
-        }]
-    }
+# @app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
+# def update_graph(stock_id):
+#     dataframe = web.DataReader(stock_id, 'naver', start='2015-01-01', end=dt.now()).reset_index()
+#     dataframe['Date'] = pd.to_datetime(dataframe['Date']).dt.date
+#     return {
+#         'data': [{
+#             'x': dataframe.Date,
+#             'y': dataframe.Close
+#         }]
+#     }
 
 if __name__ == '__main__':
     app.run_server(debug=True)
