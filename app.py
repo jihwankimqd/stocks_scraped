d3814e4c38a29657162e0f0847b0f25ae89aa6a8
Ji Hwan Kim <jihwankimqd@gmail.com>
2020-07-20 15:17:18 +0900

    first

--- app.py
+++ app.py
@@ -126,8 +126,16 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 import pandas as pd
+import pandas_datareader.data as web
+
+from datetime import datetime as dt
+
+from balance_sheet_scraper import get_balance_sheet
+
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -163,6 +171,18 @@ app.layout = html.Div([
         value='005930'
     ),
 
+    html.H2('Stock Graph'),
+    dcc.Graph(id='my-graph'),
+    html.P(''),
+
+    html.H4(children='Stock Data'),
+    html.Table(id='stock-table'),
+    html.P(''),
+
+    html.H4(children='Financial Information'),
+    html.Table(id='balance-table'),
+    html.P(''),
+
     html.Div(id='display-value')
 ])
 
@@ -170,6 +190,53 @@ app.layout = html.Div([
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(value):
     return 'You have selected "{}"'.format(value)
+
+
+
+# Stock Table
+@app.callback(Output('stock-table', 'children'), [Input('my-dropdown', 'value')])
+def generate_stock_table(stock_id, max_rows=17):
+    # Get stock data using web.DataReader(), which conveniently has a built-in function for Naver Finance
+    dataframe = web.DataReader(stock_id, 'naver', start='2015-01-01', end=dt.now()).reset_index()
+    dataframe['Date'] = pd.to_datetime(dataframe['Date']).dt.date
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[-i-1][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+# Balance Sheet
+@app.callback(Output('balance-table', 'children'), [Input('my-dropdown', 'value')])
+def generate_balance_table(stock_id, max_rows=16):
+    dataframe = get_balance_sheet(stock_id)
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[-i-1][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+# Stock Graph
+@app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
+def update_graph(stock_id):
+    dataframe = web.DataReader(stock_id, 'naver', start='2015-01-01', end=dt.now()).reset_index()
+    dataframe['Date'] = pd.to_datetime(dataframe['Date']).dt.date
+    return {
+        'data': [{
+            'x': dataframe.Date,
+            'y': dataframe.Close
+        }]
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
